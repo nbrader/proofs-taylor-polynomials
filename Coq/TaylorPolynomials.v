658ebdcb39048c08a465f263174e3fbfc9a13b18
nbrader <nbrader+github@gmail.com>
2024-09-30 22:26:53 +0100

    D_D_Lin_a_F_is_zero was redundant because it was an input to the theorem so I removed it. Added more comments too.

--- Coq/TaylorPolynomials.v
+++ Coq/TaylorPolynomials.v
@@ -21,35 +21,41 @@ Axiom D : (R -> R) -> (R -> R).
 Axiom zero_integral : forall (f : R -> R), (D f = fun x => 0) -> exists (c : R), f = fun x => c.
 Axiom constant_integral : forall (f : R -> R), forall (c : R), (D f = fun x => c) -> exists (c' : R), f = fun x => c*x + c'.
 
-(* The key property assumed of Lin a F is that it's second derivative is zero *)
-Axiom D_D_Lin_a_F_is_zero : forall (a : R), D (D (Lin a F)) = fun x => 0.
-
 (* Proof that the linearisation of a function must be the Taylor polynomial of it of degree 1. *)
 Theorem Lin_exists_uniquely : forall (a : R),
-  (forall (x : R), D (D (Lin a F)) x = 0) ->
+
+  (* The second derivative of any linearisation of F is zero. *)
+  (forall (a : R), D (D (Lin a F)) = fun x => 0) ->
+
+  (* The linearisation at a of F applied to a is equal to F applied to a. *)
   Lin a F a = F a ->
+
+  (* The first derivative of the linearisation at a of F applied to a is equal to the first derivative of F applied to a. *)
   D (Lin a F) a = D F a -> Lin a F = fun x => (D F a)*(x-a) + F a.
 Proof.
   intros.
-  pose proof (D_D_Lin_a_F_is_zero a).
+  pose proof (H a).
 
   apply (zero_integral (D (Lin a F))) in H2.
 
   destruct H2.
   assert (D (Lin a F) a = x) by (rewrite H2; reflexivity).
-  rewrite H1 in H3.
+  rewrite H1 in H3. clear H1.
 
   apply (constant_integral (Lin a F) x) in H2.
 
   destruct H2.
 
-  assert (Lin a F a = x * a + x0) by (rewrite H2; reflexivity).
-  rewrite H0 in H4.
-  rewrite <- H3 in H4.
-  assert (x0 = F a - (D F a) * a) by (rewrite H4; ring).
-  rewrite H5 in H2.
+  assert (Lin a F a = x * a + x0) by (rewrite H1; reflexivity).
+  rewrite H0 in H2. clear H0.
   rewrite <- H3 in H2.
-  assert ((fun x : R => D F a * x + (F a - D F a * a)) = (fun x : R => D F a * (x - a) + F a)) by (apply functional_extensionality; intros; ring).
-  rewrite H6 in H2.
-  apply H2.
+
+  assert (x0 = F a - (D F a) * a) by (rewrite H2; ring). clear H2.
+  rewrite H0 in H1. clear H0.
+  rewrite <- H3 in H1. clear H3.
+
+  assert (((fun x : R => D F a * x + (F a - D F a * a)) = (fun x : R => D F a * (x - a) + F a))) by (apply functional_extensionality; intros; ring).
+  rewrite H0 in H1. clear H0.
+  
+  apply H1.
 Qed.
