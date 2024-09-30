af18ff2b83043b2714e3b261c60a5053f12e00ad
nbrader <nbrader+github@gmail.com>
2024-09-30 21:54:18 +0100

    Proved that the linearisation of a function must be the Taylor polynomial of it of degree 1.

--- Coq/TaylorPolynomials.v
+++ Coq/TaylorPolynomials.v
@@ -1,4 +1,5 @@
 Require Import Coq.Reals.Reals.
+Require Import Coq.Logic.FunctionalExtensionality.
 
 Open Scope R_scope.
 
@@ -10,7 +11,7 @@ properties of differentiation I require.
 *)
 
 (* Lin f is the linearisation of f *)
-Axiom Lin : (R -> R) -> (R -> R).
+Axiom Lin : R -> (R -> R) -> (R -> R).
 
 Parameter F : R -> R.
 Parameter F_prime : R -> R.
@@ -19,18 +20,45 @@ Parameter F_prime : R -> R.
 Parameter D : (R -> R) -> (R -> R).
 
 (* Derivative properties *)
+Axiom zero_integral : forall (f : R -> R), (D f = fun x => 0) -> exists (c : R), f = fun x => c.
+Axiom constant_integral : forall (f : R -> R), forall (c : R), (D f = fun x => c) -> exists (c' : R), f = fun x => c*x + c'.
+
 Axiom D_linear : forall (f g : R -> R), D (fun x => f x + g x) = fun x => D f x + D g x.
 Axiom D_homog : forall (f : R -> R), forall (s : R), D (fun x => s * f x) = fun x => s * D f x.
 
 (* Assume the supplied functions are the true first and second order derivatives *)
 Axiom F_derivative : D F = F_prime.
-Axiom F_prime_derivative : D F_prime = fun x => 0.
 
+Axiom Lin_a_F_at_a_is_F_a : forall (a : R), D (Lin a F) a = F a.
+Axiom Lin_a_F_derivative_at_a_is_F_prime_a : forall (a : R), D (Lin a F) a = F_prime a.
+Axiom Lin_a_F_derivative_derivative_is_zero : forall (a : R), D (D (Lin a F)) = fun x => 0.
+
+(* Proof that the linearisation of a function must be the Taylor polynomial of it of degree 1. *)
 Theorem Lin_exists_uniquely : forall (a : R),
-  (forall (x : R), D (D (Lin F)) x = 0) ->
-  Lin F a = F a ->
-  D (Lin F) a = D F a -> Lin F = fun x => (D F a)*(x-a) + F a.
+  (forall (x : R), D (D (Lin a F)) x = 0) ->
+  Lin a F a = F a ->
+  D (Lin a F) a = D F a -> Lin a F = fun x => (D F a)*(x-a) + F a.
 Proof.
   intros.
+  pose proof (Lin_a_F_derivative_derivative_is_zero a).
 
-Admitted.
+  apply (zero_integral (D (Lin a F))) in H2.
+
+  destruct H2.
+  assert (D (Lin a F) a = x) by (rewrite H2; reflexivity).
+  rewrite H1 in H3.
+
+  apply (constant_integral (Lin a F) x) in H2.
+
+  destruct H2.
+
+  assert (Lin a F a = x * a + x0) by (rewrite H2; reflexivity).
+  rewrite H0 in H4.
+  rewrite <- H3 in H4.
+  assert (x0 = F a - (D F a) * a) by (rewrite H4; ring).
+  rewrite H5 in H2.
+  rewrite <- H3 in H2.
+  assert ((fun x : R => D F a * x + (F a - D F a * a)) = (fun x : R => D F a * (x - a) + F a)) by (apply functional_extensionality; intros; ring).
+  rewrite H6 in H2.
+  apply H2.
+Qed.
