e015f5845718405d1c37c9984793634d18e2198c
nbrader <nbrader+github@gmail.com>
2024-09-30 23:12:38 +0100

    Chunked the proof better.

--- Coq/TaylorPolynomials.v
+++ Coq/TaylorPolynomials.v
@@ -36,24 +36,19 @@ Proof.
   intros a second_deriv_Lin_0 equals_F_at_a deriv_equals_deriv_F_at_a.
 
   apply (zero_integral (D (Lin a F))) in second_deriv_Lin_0.
-
   destruct second_deriv_Lin_0 as [x first_deriv_Lin_c].
   assert (linear_coeff_def : D (Lin a F) a = x) by (rewrite first_deriv_Lin_c; reflexivity).
-
   rewrite deriv_equals_deriv_F_at_a in linear_coeff_def. clear deriv_equals_deriv_F_at_a.
-
+  
   apply (constant_integral (Lin a F) x) in first_deriv_Lin_c.
-
   destruct first_deriv_Lin_c as [x0 Lin_def].
-
   assert (algebra_1 : Lin a F a = x * a + x0) by (rewrite Lin_def; reflexivity).
   rewrite equals_F_at_a in algebra_1. clear equals_F_at_a.
   rewrite <- linear_coeff_def in algebra_1.
-
   assert (constant_term_def : x0 = F a - (D F a) * a) by (rewrite algebra_1; ring). clear algebra_1.
+
   rewrite constant_term_def in Lin_def. clear constant_term_def.
   rewrite <- linear_coeff_def in Lin_def. clear linear_coeff_def.
-
   assert (algebra_2 : ((fun x : R => D F a * x + (F a - D F a * a)) = (fun x : R => D F a * (x - a) + F a))) by (apply functional_extensionality; intros; ring).
   rewrite algebra_2 in Lin_def. clear algebra_2.
 
