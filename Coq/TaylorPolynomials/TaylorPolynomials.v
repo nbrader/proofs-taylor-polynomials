775668f1112d7cdebca055ff0918ad16b2ffa0bb
nbrader <nbrader+github@gmail.com>
2024-10-15 17:49:36 +0100

    Removed some unused assumptions

--- Coq/TaylorPolynomials/TaylorPolynomials.v
+++ Coq/TaylorPolynomials/TaylorPolynomials.v
@@ -662,23 +662,16 @@ Theorem nth_integral_of_zero :
   forall (D : (R -> R) -> (R -> R)),
 
   (* Derivative properties *)
-  forall (zero_integral : forall (f : R -> R), (D f = fun x => 0) <-> exists (c : R), f = fun x => c),
   forall (constant_integral : forall (f : R -> R) (c : R), (D f = fun x => c) <-> exists (c' : R), f = fun x => c*x + c'),
   forall (D_additive : forall (f g : R -> R), D (fun x => f x + g x) = fun x => D f x + D g x),
   forall (D_homog : forall (f : R -> R), forall (s : R), D (fun x => s * f x) = fun x => s * D f x),
   forall (D_product_rule : forall (f g : R -> R), D (fun x => f x * g x) = fun x => D f x * g x + f x * D g x),
   forall (integration_constant : forall (f g : R -> R), D f = D g -> exists (c : R), f = (fun x : R => g x + c)), (* <-- Not true for functions with discontinuities *)
 
-  (* The (n+1)th derivative of any Taylor polynomial of degree n of F is zero *)
-  (forall (n : nat) (a : R) (F : R -> R), iter D (S n) (Taylor n a F) = fun x => 0) ->
-
-  (* The mth derivative of the Taylor polynomial of degree n at a where m <= n is equal to the mth derivative of F applied to a *)
-  (forall (m n : nat) (a : R) (F : R -> R), (INR m <= INR n) -> iter D m (Taylor n a F) a = iter D m F a) ->
-
   (* The implementation of the Taylor polynomial of degree n at a for F must be the sum of the first n terms of the Taylor series: *)
   forall (n : nat) (f : R -> R), iter D n f = (fun _ => 0) -> exists (c_ : nat -> R), f = summation (fun i x' => (c_ i) * x'^i) n.
 Proof.
-  intros Taylor D zero_integral constant_integral D_additive D_homog D_product_rule integration_constant Taylor_degree Taylor_agrees_at_a n.
+  intros Taylor D constant_integral D_additive D_homog D_product_rule integration_constant n.
   induction n.
 
   - (* Base case: n = 0 *)
